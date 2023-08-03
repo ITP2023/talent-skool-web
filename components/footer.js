bb0e465a07ba837acd3b3449bb8113e33c8031bc
Ihab P <113042103+ITP2023@users.noreply.github.com>
2023-08-03 22:05:17 +0300

    added seperate icon components and made footer changes

--- components/footer.js
+++ components/footer.js
@@ -1,22 +1,28 @@
 import Image from "next/image";
-
+import TwitterIcon from "./twitter_icon";
+import FacebookIcon from "./facebook_icon";
 
 export default function Footer() {
   return (
-    <section className="bg-indigo-600 lg:bg-[url('/footer.svg')] w-full">
+    <section className="mt-20 bg-indigo-600 lg:bg-[url('/footer.svg')] w-full">
       <div className="relative flex justify-start md:justify-center md:items-end w-full">
-        <div className="flex pt-36 md:pt-32 lg:pt-40 xl:pt-96 px-4 md:px-6  xl:px-20 flex-col justify-start items-start md:justify-center md:items-center relative z-10">
+        <div className="flex py-14 md:pt-32 lg:pt-40 xl:pt-96 px-4 md:px-6  xl:px-20 flex-col justify-start items-start md:justify-center md:items-center relative z-10">
           <div className="flex flex-col items-start justify-start xl:justify-center xl:space-x-8 xl:flex-row">
-            
             <div className="mt-12 xl:mt-0 grid grid-cols-1 sm:grid-cols-3 gap-y-12 sm:gap-y-0 w-full md:w-auto sm:gap-x-20 md:gap-x-28 xl:gap-8">
               <div className="sm:w-40 md:w-auto xl:w-72 flex justify-start items-start flex-col space-y-6">
-                <h2 className="text-base xl:text-xl font-bold xl:font-semibold leading-4 xl:leading-5 text-white underline underline-offset-2">Talentskool</h2>
-                <p className="text-white"> Talent Skool is best e-Learning
- hub with different types of
- courses.  </p>
+                <h2 className="text-base xl:text-xl font-bold xl:font-semibold leading-4 xl:leading-5 text-white underline underline-offset-2">
+                  Talentskool
+                </h2>
+                <p className="text-white">
+                  {" "}
+                  Talent Skool is best e-Learning hub with different types of
+                  courses.{" "}
+                </p>
               </div>
               <div className="sm:w-40 md:w-auto xl:w-72 flex justify-start items-start flex-col space-y-6">
-                <h2 className="text-base xl:text-xl font-bold xl:font-semibold leading-4 xl:leading-5 text-white">Courses</h2>
+                <h2 className="text-base xl:text-xl font-bold xl:font-semibold leading-4 xl:leading-5 text-white">
+                  Courses
+                </h2>
                 <button className="text-left text-base hover:text-gray-400 leading-none text-gray-100">
                   App Design
                 </button>
@@ -34,7 +40,9 @@ export default function Footer() {
                 </button>
               </div>
               <div className=" xl:w-72 flex justify-start items-start flex-col space-y-6">
-                <h2 className="text-base xl:text-xl font-bold xl:font-semibold leading-4 xl:leading-5 text-white">Company</h2>
+                <h2 className="text-base xl:text-xl font-bold xl:font-semibold leading-4 xl:leading-5 text-white">
+                  Company
+                </h2>
                 <button className="text-base text-left hover:text-gray-400 leading-none text-gray-100">
                   About Us
                 </button>
@@ -50,40 +58,33 @@ export default function Footer() {
                 <button className="text-base text-left hover:text-gray-400 leading-none text-gray-100">
                   Leave a feedback
                 </button>
+                <button className="text-base leading-none text-white hover:text-gray-300">
+                  Terms of service
+                </button>
+                <button className="text-base leading-none text-white hover:text-gray-300">
+                  Privacy Policy
+                </button>
+                <button className="text-base leading-none text-white hover:text-gray-300">
+                  Security
+                </button>
               </div>
             </div>
           </div>
-          <div className="mt-12 flex  xl:justify-between xl:flex-row flex-col-reverse items-center xl:items-start w-full ">
-            <p className="mt-10 md:mt-12 xl:mt-0 text-sm leading-none text-white">2023 TalentSkool &copy; All Rights Reserved</p>
-            <div className="mt-10 md:mt-12 xl:mt-0 md:flex-row flex-col flex md:justify-center w-full md:w-auto justify-start items-start space-y-4 md:space-y-0 md:items-center md:space-x-4 xl:space-x-6">
-              <button className="text-base leading-none text-white hover:text-gray-300">
-                Terms of service
-              </button>
-              <button className="text-base leading-none text-white hover:text-gray-300">
-                Privacy Policy
-              </button>
-              <button className="text-base leading-none text-white hover:text-gray-300">
-                Security
-              </button>
-
-            </div>
-            <div className="flex  justify-start md:justify-end items-start  w-full md:w-auto md:items-center space-x-6 ">
+          <div className="flex xl:justify-between xl:flex-row flex-col items-center xl:items-start w-full ">
+            <p className="mt-10 md:mt-12 xl:mt-0 text-sm leading-none text-white">
+              2023 TalentSkool &copy; All Rights Reserved
+            </p>
+            <div className="flex justify-center md:justify-end items-start mt-10 w-full md:w-auto md:items-center space-x-6 ">
               <button className="text-white hover:text-gray-200 w-6">
-                <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M23.25 5.13282C22.406 5.49955 21.513 5.74116 20.5992 5.85001C21.5595 5.28769 22.2817 4.39434 22.6303 3.33751C21.7224 3.86841 20.7307 4.24092 19.6978 4.43907C19.2629 3.98322 18.7397 3.62059 18.1603 3.3732C17.5808 3.12581 16.9571 2.99884 16.327 3.00001C13.7761 3.00001 11.7117 5.03438 11.7117 7.5422C11.7099 7.89102 11.7499 8.23881 11.8308 8.57813C10.0016 8.49238 8.2104 8.02575 6.57187 7.2081C4.93333 6.39044 3.48351 5.23977 2.31516 3.8297C1.90527 4.52069 1.6885 5.30909 1.6875 6.11251C1.6875 7.68751 2.50922 9.0797 3.75 9.89532C3.01487 9.87787 2.29481 9.68331 1.65094 9.32813V9.38438C1.65094 11.5875 3.24469 13.4203 5.35406 13.8375C4.9574 13.9433 4.54864 13.9968 4.13812 13.9969C3.84683 13.9974 3.5562 13.9691 3.27047 13.9125C3.85687 15.7172 5.56359 17.0297 7.58531 17.0672C5.94252 18.3333 3.9256 19.0175 1.85156 19.0125C1.48341 19.012 1.11561 18.99 0.75 18.9469C2.85993 20.2942 5.31255 21.0068 7.81594 21C16.3172 21 20.9616 14.0766 20.9616 8.07188C20.9616 7.87501 20.9564 7.67813 20.947 7.48595C21.8485 6.84472 22.6283 6.04787 23.25 5.13282V5.13282Z" fill="currentColor" />
-                </svg>
+                <TwitterIcon />
               </button>
               <button className="text-white hover:text-gray-200 w-6">
-                <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" clipRule="evenodd" d="M22.5 12.0645C22.5 6.26602 17.7984 1.56445 12 1.56445C6.20156 1.56445 1.5 6.26602 1.5 12.0645C1.5 17.3051 5.33906 21.649 10.3594 22.4374V15.1005H7.69266V12.0645H10.3594V9.75117C10.3594 7.12008 11.9273 5.66555 14.3255 5.66555C15.4744 5.66555 16.6763 5.87086 16.6763 5.87086V8.45508H15.3516C14.048 8.45508 13.6402 9.26414 13.6402 10.0957V12.0645H16.552L16.087 15.1005H13.6406V22.4384C18.6609 21.6504 22.5 17.3065 22.5 12.0645Z" fill="currentColor" />
-                </svg>
+                <FacebookIcon />
               </button>
-
             </div>
           </div>
         </div>
       </div>
     </section>
-
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -8,41 +8,8 @@ import Navbar from "../components/navbar";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "@/firebaseMod";
 
-const CancelIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="w-5 h-5 mx-auto"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fillRule="evenodd"
-        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-};
 
-const EmailIcon = () => {
-  return (
-    <svg
-      className="w-6 h-6 text-gray-400 absolute left-3 inset-y-0 my-auto"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-      />
-    </svg>
-  );
-};
+
 
 const FaqsCard = (props) => {
   const answerElRef = useRef();
