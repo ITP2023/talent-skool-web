9388c68abf11d99266fd4060464a01712f116cfa
HarshaHari1207 <harsha733084@gmailc.om>
2023-08-12 19:32:47 +0530

    Font family configured with tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -116,7 +116,10 @@ module.exports = {
       md: "1060px",
       lg: "1200px",
       xl: "1700px"
-    }
+    },
+    fontFamily: {
+      sans: ['Arial', 'sans-serif'],
+    },
   },
   
   plugins: [],
