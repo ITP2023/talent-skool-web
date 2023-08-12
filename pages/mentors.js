a3fc2d5893bc6a340644b292da5312db0e18a5da
HarshaHari1207 <harsha733084@gmailc.om>
2023-08-12 10:31:43 +0530

    Empty Mentors page.

--- pages/mentors.js
+++ pages/mentors.js
@@ -1,5 +1,29 @@
-const MentorsPage = () => {
+import Head from "next/head";
+// import Image from "next/image";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
+const People = () =>{
+
+    return(
+        <div className="w-full h-full">
+
+        </div>
+    )
+}
+
+
+const MentorsPage = () => {
+    return (
+        <div className="absolute bg-black w-full h-full">
+          <Head>
+            <title>Mentor</title>
+          </Head>
+          <Navbar />
+          <People />
+          <Footer />
+        </div>
+      );
 }
 
 export default MentorsPage;
