5e26c1e86420ea6da3550dd0639c78efa93627df
Ihab P <113042103+ITP2023@users.noreply.github.com>
2023-08-12 15:43:33 +0300

    formated mentors page

--- pages/mentors.js
+++ pages/mentors.js
@@ -3,27 +3,21 @@ import Head from "next/head";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const People = () =>{
-
-    return(
-        <div className="w-full h-full">
-
-        </div>
-    )
-}
-
+const People = () => {
+  return <div className="w-full h-full"></div>;
+};
 
 const MentorsPage = () => {
-    return (
-        <div className="absolute bg-black w-full h-full">
-          <Head>
-            <title>Mentor</title>
-          </Head>
-          <Navbar />
-          <People />
-          <Footer />
-        </div>
-      );
-}
+  return (
+    <div className="absolute bg-black w-full h-full">
+      <Head>
+        <title>Mentor</title>
+      </Head>
+      <Navbar />
+      <People />
+      <Footer />
+    </div>
+  );
+};
 
 export default MentorsPage;
